26af26ed76ead2488cf3d1895b28dea8f541b1e2
Harald Radke <rat@comnets.rwth-aachen.de>
2009-09-17 19:33:47 +0200

    MPs now wait with proxy self-registration on beacons till a portal is reachable

--- src/pathselection/BeaconLinkQualityMeasurement.cpp
+++ src/pathselection/BeaconLinkQualityMeasurement.cpp
@@ -146,7 +146,7 @@ BroadcastLinkQuality::newPeerMeasurement(double peerSuccessRate, wns::Ratio peer
         linkCreated = true;
     }
 
-    if ( ps->getNextHop(myAddress, peerAddress) != wns::service::dll::UnicastAddress() and ps->getProxyFor(parent->getFUN()->getLayer<dll::Layer2*>()->getDLLAddress()) == wns::service::dll::UnicastAddress())
+    if ( ps->getNextHop(myAddress, peerAddress) != wns::service::dll::UnicastAddress() and ps->getProxyFor(parent->getFUN()->getLayer<dll::Layer2*>()->getDLLAddress()) == wns::service::dll::UnicastAddress() and (ps->getPortalFor(peerAddress) != wns::service::dll::UnicastAddress() or ps->isPortal(peerAddress)))
     {
 
         MESSAGE_SINGLE(NORMAL, parent->logger,"Registering mesh point as proxy for itself at VPS");
