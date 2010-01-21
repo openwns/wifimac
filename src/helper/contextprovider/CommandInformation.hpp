091d7801c94039934af5ed51796603fef35eb05f
Sebastian Max <smx@comnets.rwth-aachen.de>
2010-01-21 15:17:33 +0100

    Moved CommandInformationContext into wns::probe::bus

--- src/helper/contextprovider/CommandInformation.hpp
+++ src/helper/contextprovider/CommandInformation.hpp
@@ -37,48 +37,12 @@
 #include <WNS/service/dll/Address.hpp>
 #include <WIFIMAC/Layer2.hpp>
 
-#include <WNS/probe/bus/CompoundContextProvider.hpp>
-#include <WNS/ldk/CommandReaderInterface.hpp>
-#include <WNS/ldk/fun/FUN.hpp>
+#include <WNS/probe/bus/CommandContextProvider.hpp>
 
-#include <WNS/isClass.hpp>
+//#include <WNS/isClass.hpp>
 
 namespace wifimac { namespace helper { namespace contextprovider {
 
-	/** @brief Base class for all ContextProviders which use a specific command in
-	 * the compound to determine the context
-	 * input variables:
-	 *   commandName: the name of the command where the information is readable
-     *   specificKey: the name of the key which allows context-filtering
-	 *                afterwards. Use a key starting with 'MAC.Compound' so that
-	 *                it is clear that only compound-based probes (e.g. Packet)
-	 *                can use this filter
-	 */
-	class CommandInformationContext:
-		virtual public wns::probe::bus::CompoundContextProvider
-	{
-	public:
-		CommandInformationContext(wns::ldk::fun::FUN* fun, std::string commandName, std::string specificKey):
-			commandReader(fun->getCommandReader(commandName)),
-			key(specificKey)
-			{}
-
-		~CommandInformationContext() {}
-
-		virtual const std::string&
-		getKey() const
-			{
-				return this->key;
-			}
-	protected:
-		wns::ldk::CommandReaderInterface* commandReader;
-		const std::string key;
-    private:
-        virtual void 
-        doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const = 0;
-
-
-	};
 
     /**
 	 * @brief Context provider for a given compound: 1 if the compound is a
@@ -87,63 +51,50 @@ namespace wifimac { namespace helper { namespace contextprovider {
 	 * The status is determined by checking if the target address is valid
      *
 	 */
-	class IsUnicast:
-		virtual public CommandInformationContext
-	{
-	public:
-		IsUnicast(wns::ldk::fun::FUN* fun, std::string ucCommandName):
-			CommandInformationContext(fun, ucCommandName, "MAC.CompoundIsUnicast")
-			{}
-
-		virtual
-		~IsUnicast() {}
-
+    class IsUnicast:
+        virtual public wns::probe::bus::CommandContextProvider<dll::UpperCommand>
+    {
+    public:
+        IsUnicast(wns::ldk::fun::FUN* fun, std::string ucCommandName):
+            wns::probe::bus::CommandContextProvider<dll::UpperCommand>(fun, ucCommandName, "MAC.CompoundIsUnicast")
+            {};
     private:
-		virtual void
-		doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
-		{
-            assure(compound, "Received NULL CompoundPtr");
-            if(commandReader->commandIsActivated(compound->getCommandPool()))
+        virtual void
+        doVisitCommand(wns::probe::bus::IContext& c, const dll::UpperCommand* command) const
             {
-                if(commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.targetMACAddress.isValid())
+                if(command->peer.targetMACAddress.isValid())
                 {
-                    c.insertInt(this->key, 1);
+                    c.insertInt(getKey(), 1);
                 }
                 else
                 {
-                    c.insertInt(this->key, 0);
+                    c.insertInt(getKey(), 0);
                 }
-            }
-		}
-	};
+            };
+    };
 
-	/**
+    /**
 	 * @brief Context provider for a given compound: 1 if the compound is
 	 *   addressed to me, 0 otherwise
 	 *
 	 * The status is determined by checking if the compound is either broadcast
-	 * or, if not, the targetAddress in the upperConvergenceCommand is one of my transceivers.
+	 * or, if not, the targetAddress in the upperConvergenceCommand is one of my
+	 * transceivers.
 	 */
-	class IsForMe:
-		virtual public CommandInformationContext
-	{
-	public:
-		IsForMe(wns::ldk::fun::FUN* fun_, std::string ucCommandName):
-			CommandInformationContext(fun_, ucCommandName, "MAC.CompoundIsForMe"),
-			fun(fun_)
-			{}
-
-		~IsForMe() {}
+    class IsForMe:
+        virtual public wns::probe::bus::CommandContextProvider<dll::UpperCommand>
+    {
+    public:
+        IsForMe(wns::ldk::fun::FUN* fun_, std::string ucCommandName):
+            wns::probe::bus::CommandContextProvider<dll::UpperCommand>(fun_, ucCommandName, "MAC.CompoundIsForMe"),
+            fun(fun_)
+            {};
 
     private:
-		virtual void
-		doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
-		{
-            assure(compound, "Received NULL CompoundPtr");
-            if(commandReader->commandIsActivated(compound->getCommandPool()))
+        virtual void
+        doVisitCommand(wns::probe::bus::IContext& c, const dll::UpperCommand* command) const
             {
-                wns::service::dll::UnicastAddress targetAddress =
-                    commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.targetMACAddress;
+                wns::service::dll::UnicastAddress targetAddress = command->peer.targetMACAddress;
                 if(targetAddress.isValid())
                 {
                     if(fun->getLayer<wifimac::Layer2*>()->isTransceiverMAC(targetAddress))
@@ -160,39 +111,31 @@ namespace wifimac { namespace helper { namespace contextprovider {
                     // broadcast -> is for me
                     c.insertInt(this->key, 1);
                 }
-            }
-		}
-
+            };
         wns::ldk::fun::FUN* fun;
-	};
+    };
 
-	/**
+    /**
 	 * @brief Context provider for a given compound: 1 if the compound is
-	 *   send by me, 0 otherwise
-	 *
-	 * The status is determined by checking if the compound is either broadcast
-	 * or, if not, the sourceAddress in the upperConvergenceCommand is one of my transceivers.
-	 */
-	class IsFromMe:
-		virtual public CommandInformationContext
-	{
-	public:
-		IsFromMe(wns::ldk::fun::FUN* fun_, std::string ucCommandName):
-			CommandInformationContext(fun_, ucCommandName, "MAC.CompoundIsFromMe"),
-			fun(fun_)
-			{}
-
-		~IsFromMe() {}
+     *   send by me, 0 otherwise
+     *
+     * The status is determined by checking if the compound is either broadcast
+     * or, if not, the sourceAddress in the upperConvergenceCommand is one of my transceivers.
+     */
+    class IsFromMe:
+        virtual public wns::probe::bus::CommandContextProvider<dll::UpperCommand>
+    {
+    public:
+        IsFromMe(wns::ldk::fun::FUN* fun_, std::string ucCommandName):
+            wns::probe::bus::CommandContextProvider<dll::UpperCommand>(fun_, ucCommandName, "MAC.CompoundIsFromMe"),
+            fun(fun_)
+            {};
 
     private:
-		virtual void
-		doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
-		{
-            assure(compound, "Received NULL CompoundPtr");
-            if(commandReader->commandIsActivated(compound->getCommandPool()))
+        virtual void
+        doVisitCommand(wns::probe::bus::IContext& c, const dll::UpperCommand* command) const
             {
-                wns::service::dll::UnicastAddress sourceAddress =
-                    commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.sourceMACAddress;
+                wns::service::dll::UnicastAddress sourceAddress = command->peer.sourceMACAddress;
                 if(sourceAddress.isValid())
                 {
                     if(fun->getLayer<wifimac::Layer2*>()->isTransceiverMAC(sourceAddress))
@@ -209,149 +152,110 @@ namespace wifimac { namespace helper { namespace contextprovider {
                     // broadcast -> is not from me
                     c.insertInt(this->key, 0);
                 }
-            }
-		}
-
+            };
         wns::ldk::fun::FUN* fun;
-	};
+    };
 
-	/**
+    /**
 	 * @brief Context provider for a given compound: Filters by the source
      *	address given in the upperConvergenceComand
 	 */
-	class SourceAddress:
-		virtual public CommandInformationContext
-	{
-	public:
-		SourceAddress(wns::ldk::fun::FUN* fun, std::string ucCommandName):
-			CommandInformationContext(fun, ucCommandName, "MAC.CompoundSourceAddress")
-			{}
-
-		virtual
-		~SourceAddress() {}
+    class SourceAddress:
+        virtual public wns::probe::bus::CommandContextProvider<dll::UpperCommand>
+    {
+    public:
+        SourceAddress(wns::ldk::fun::FUN* fun, std::string ucCommandName):
+            wns::probe::bus::CommandContextProvider<dll::UpperCommand>(fun, ucCommandName, "MAC.CompoundSourceAddress")
+            {};
 
     private:
-		virtual void
-		doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
-		{
-            assure(compound, "Received NULL CompoundPtr");
+        virtual void
+        doVisitCommand(wns::probe::bus::IContext& c, const dll::UpperCommand* command) const
+            {
+                if(command->peer.sourceMACAddress.isValid())
+                {
+                    // if the command is activated, we add the tx address to the
+                    // context
+                    c.insertInt(this->key,
+                                command->peer.sourceMACAddress.getInteger());
+                }
+            }
+    };
 
-			if(commandReader->commandIsActivated(compound->getCommandPool()) == true)
-			{
-				if(commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.sourceMACAddress.isValid())
-				{
-					// and if the command is activated, we add the tx
-					// address to the context
-					c.insertInt(this->key,
-					commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.sourceMACAddress.getInteger());
-				}
-			}
-		}
-	};
-
-	/**
+    /**
 	 * @brief Context provider for a given compound: Filters by the target
      *	address given in the upperConvergenceComand
 	 */
-	class TargetAddress:
-		virtual public CommandInformationContext
-	{
-	public:
-		TargetAddress(wns::ldk::fun::FUN* fun, std::string ucCommandName):
-			CommandInformationContext(fun, ucCommandName, "MAC.CompoundTargetAddress")
-			{}
-
-		~TargetAddress() {}
+    class TargetAddress:
+        virtual public wns::probe::bus::CommandContextProvider<dll::UpperCommand>
+    {
+    public:
+        TargetAddress(wns::ldk::fun::FUN* fun, std::string ucCommandName):
+            wns::probe::bus::CommandContextProvider<dll::UpperCommand>(fun, ucCommandName, "MAC.CompoundTargetAddress")
+            {};
 
     private:
-		virtual void
-		doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
-		{
-            assure(compound, "Received NULL CompoundPtr");
+        virtual void
+        doVisitCommand(wns::probe::bus::IContext& c, const dll::UpperCommand* command) const
+            {
+                if(command->peer.targetMACAddress.isValid())
+                {
+                    // if the command is activated, we add the tx address to the
+                    // context
+                    c.insertInt(this->key,
+                                command->peer.targetMACAddress.getInteger());
+                }
+            }
+    };
 
-			if(commandReader->commandIsActivated(compound->getCommandPool()) == true)
-			{
-    			if(commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.targetMACAddress.isValid())
-				{
-					// and if the command is activated, we add the tx
-					// address to the context
-					c.insertInt(this->key,
-					commandReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.targetMACAddress.getInteger());
-				}
-			}
-		}
-	};
-
-	/**
+    /**
 	 * @brief Context provider for a given compound: Filters by number of hops
 	 *    that the compound has travelled.
 	 *
 	 * The information is read from the magic.hopCount in the forwarding-Command
 	 */
-	class HopCount :
-			virtual public CommandInformationContext
-	{
-	public:
-		HopCount(wns::ldk::fun::FUN* fun, std::string forwardingCommandName):
-			CommandInformationContext(fun, forwardingCommandName, "MAC.CompoundHopCount")
-			{}
-
-		virtual
-		~HopCount() {}
-    
+    class HopCount :
+        virtual public wns::probe::bus::CommandContextProvider<wifimac::pathselection::ForwardingCommand>
+    {
+    public:
+        HopCount(wns::ldk::fun::FUN* fun, std::string forwardingCommandName):
+            wns::probe::bus::CommandContextProvider<wifimac::pathselection::ForwardingCommand>(fun, forwardingCommandName, "MAC.CompoundHopCount")
+            {};
     private:
-		virtual void
-		doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
-		{
-            assure(compound, "Received NULL CompoundPtr");                
+        virtual void
+        doVisitCommand(wns::probe::bus::IContext& c, const wifimac::pathselection::ForwardingCommand* command) const
+            {
+                if(command->magic.hopCount > 0)
+                {
+                    // if the command is activated, we add the tx address to the
+                    // context
+                    c.insertInt(this->key,
+                                command->magic.hopCount);
+                }
+            }
+    };
 
-			if(commandReader->commandIsActivated(compound->getCommandPool()) == true)
-			{
-				if(commandReader->
-				   readCommand<wifimac::pathselection::ForwardingCommand>
-				   (compound->getCommandPool())->magic.hopCount > 0)
-				{
-					// and if the command is activated, we add the tx
-					// address to the context
-					c.insertInt(this->key,
-						commandReader->
-						readCommand<wifimac::pathselection::ForwardingCommand>
-						(compound->getCommandPool())->magic.hopCount);
-				}
-			}
-		}
-	};
-
-	/**
+    /**
 	 * @brief Context provider for a given compound: Filters by the index of the
 	 *    MCS with which the compound was send (will be send)
 	 *
 	 * The information is read from the phyUserCommand
 	 */
     class DataBitsPerSymbol:
-        virtual public CommandInformationContext
+        virtual public wns::probe::bus::CommandContextProvider<wifimac::lowerMAC::ManagerCommand>
     {
     public:
         DataBitsPerSymbol(wns::ldk::fun::FUN* fun, std::string managerCommandName):
-            CommandInformationContext(fun, managerCommandName, "MAC.CompoundDBPS")
-            {}
-
-        virtual
-        ~DataBitsPerSymbol() {}
+            wns::probe::bus::CommandContextProvider<wifimac::lowerMAC::ManagerCommand>(fun, managerCommandName, "MAC.CompoundDBPS")
+            {};
 
     private:
         virtual void
-        doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
+        doVisitCommand(wns::probe::bus::IContext& c, const wifimac::lowerMAC::ManagerCommand* command) const
             {
-            assure(compound, "Received NULL CompoundPtr");
-
-            if(commandReader->commandIsActivated(compound->getCommandPool()) == true)
-            {
-                wifimac::convergence::PhyMode phymode = commandReader->readCommand<wifimac::lowerMAC::ManagerCommand>
-                    (compound->getCommandPool())->getPhyMode();
+                wifimac::convergence::PhyMode phymode = command->getPhyMode();
                 c.insertInt(this->key, phymode.getDataBitsPerSymbol());
             }
-        }
     };
 
     /**
@@ -361,29 +265,20 @@ namespace wifimac { namespace helper { namespace contextprovider {
 	 * The information is read from the phyUserCommand
 	 */
     class SpatialStreams :
-        virtual public CommandInformationContext
+        virtual public wns::probe::bus::CommandContextProvider<wifimac::lowerMAC::ManagerCommand>
     {
     public:
         SpatialStreams(wns::ldk::fun::FUN* fun, std::string managerCommandName):
-            CommandInformationContext(fun, managerCommandName, "MAC.CompoundSpatialStreams")
-            {}
-
-        virtual
-        ~SpatialStreams() {}
+            wns::probe::bus::CommandContextProvider<wifimac::lowerMAC::ManagerCommand>(fun, managerCommandName, "MAC.CompoundSpatialStreams")
+            {};
 
     private:
         virtual void
-        doVisit(wns::probe::bus::IContext& c, const wns::ldk::CompoundPtr& compound) const
+        doVisitCommand(wns::probe::bus::IContext& c, const wifimac::lowerMAC::ManagerCommand* command) const
             {
-            assure(compound, "Received NULL CompoundPtr");
-
-            if(commandReader->commandIsActivated(compound->getCommandPool()) == true)
-            {
-                wifimac::convergence::PhyMode mcs = commandReader->readCommand<wifimac::lowerMAC::ManagerCommand>
-                    (compound->getCommandPool())->getPhyMode();
+                wifimac::convergence::PhyMode mcs = command->getPhyMode();
                 c.insertInt(this->key, mcs.getNumberOfSpatialStreams());
             }
-        }
     };
 }}}
 
