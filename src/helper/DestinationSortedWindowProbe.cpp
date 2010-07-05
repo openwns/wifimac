6db83a93429a2f562b26436c4abaf3c9ab68027f
Sebastian Max <smx@comnets.rwth-aachen.de>
2010-07-05 16:48:10 +0200

    Added files...

---
+++ src/helper/DestinationSortedWindowProbe.cpp
@@ -0,0 +1,204 @@
+/******************************************************************************
+ * WiFiMac                                                                    *
+ * This file is part of openWNS (open Wireless Network Simulator)
+ * _____________________________________________________________________________
+ *
+ * Copyright (C) 2004-2007
+ * Chair of Communication Networks (ComNets)
+ * Kopernikusstr. 16, D-52074 Aachen, Germany
+ * phone: ++49-241-80-27910,
+ * fax: ++49-241-80-22242
+ * email: info@openwns.org
+ * www: http://www.openwns.org
+ * _____________________________________________________________________________
+ *
+ * openWNS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 2 as published by the
+ * Free Software Foundation;
+ *
+ * openWNS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include <WIFIMAC/helper/DestinationSortedWindowProbe.hpp>
+#include <WIFIMAC/Layer2.hpp>
+
+#include <WNS/StaticFactory.hpp>
+#include <WNS/probe/bus/utils.hpp>
+#include <WNS/service/dll/StationTypes.hpp>
+
+using namespace wifimac::helper;
+
+STATIC_FACTORY_REGISTER_WITH_CREATOR(
+    DestinationSortedWindowProbe,
+    wns::ldk::probe::Probe,
+    "wifimac.helper.DestinationSortedWindowProbe",
+    wns::ldk::FUNConfigCreator);
+
+STATIC_FACTORY_REGISTER_WITH_CREATOR(
+    DestinationSortedWindowProbe,
+    wns::ldk::FunctionalUnit,
+    "wifimac.helper.DestinationSortedWindowProbe",
+    wns::ldk::FUNConfigCreator);
+
+DestinationSortedWindowProbe::DestinationSortedWindowProbe(wns::ldk::fun::FUN* fun, const wns::pyconfig::View& config_) :
+    wns::ldk::probe::bus::Window(fun, config_),
+    config(config_),
+    ucReader(NULL),
+    windowSize(config.get<simTimeType>("windowSize")),
+    ucCommandName(config.get<std::string>("ucCommandName"))
+{
+    // read the localContext from the config
+    wns::probe::bus::ContextProviderCollection localContext(&fun->getLayer()->getContextProviderCollection());
+    for (int ii = 0; ii<config.len("localIDs.keys()"); ++ii)
+    {
+        std::string key = config.get<std::string>("localIDs.keys()",ii);
+        unsigned long int value  = config.get<unsigned long int>("localIDs.values()",ii);
+        localContext.addProvider(wns::probe::bus::contextprovider::Constant(key, value));
+    }
+
+    this->bitsIncoming = wns::probe::bus::collector(localContext, config, "incomingBitDestinationSortedWindowProbeName");
+    this->bitsAggregated = wns::probe::bus::collector(localContext, config, "aggregatedBitDestinationSortedWindowProbeName");
+    this->bitsOutgoing = wns::probe::bus::collector(localContext, config, "outgoingBitDestinationSortedWindowProbeName");
+    this->relativeGoodput = wns::probe::bus::collector(localContext, config, "relativeGoodputDestinationSortedWindowProbeName");
+}
+
+DestinationSortedWindowProbe::~DestinationSortedWindowProbe()
+{
+}
+
+void
+DestinationSortedWindowProbe::onFUNCreated()
+{
+    // Obtain upper convergence command reader
+    ucReader = getFUN()->getCommandReader(ucCommandName);
+}
+
+void
+DestinationSortedWindowProbe::processIncoming(const wns::ldk::CompoundPtr& compound)
+{
+    // read the addresses
+    unsigned int sourceAddress =
+        ucReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.sourceMACAddress.getInteger();
+    unsigned int destAddress =
+        getFUN()->getLayer<wifimac::Layer2*>()->getDLLAddress().getInteger();
+
+    Bit commandPoolSize;
+    Bit dataSize;
+    this->getFUN()->calculateSizes(compound->getCommandPool(), commandPoolSize, dataSize, this);
+    const long int compoundLength = commandPoolSize + dataSize;
+
+    this->putProbe(bitsIncomingProbeHolder, sourceAddress, compoundLength);
+
+    wns::ldk::probe::bus::WindowCommand* command = this->getCommand(compound->getCommandPool());
+    wifimac::helper::DestinationSortedWindowProbe* peerFU = dynamic_cast<wifimac::helper::DestinationSortedWindowProbe*>(command->magic.probingFU);
+    assure(peerFU != NULL, "Expected wifimac::helper::DestinationSortedWindowProbe as peer!");
+
+    peerFU->putProbe(peerFU->bitsAggregatedProbeHolder, destAddress, compoundLength);
+
+    // Now do the general (base class stuff)
+    this->wns::ldk::probe::bus::Window::processIncoming(compound);
+}
+
+void
+DestinationSortedWindowProbe::processOutgoing(const wns::ldk::CompoundPtr& compound)
+{
+    // First do the general (base class) stuff
+    this->wns::ldk::probe::bus::Window::processOutgoing(compound);
+
+    wns::service::dll::UnicastAddress destAdr =
+        ucReader->readCommand<dll::UpperCommand>(compound->getCommandPool())->peer.targetMACAddress;
+
+    assure(destAdr.isValid() or
+           this->getFUN()->getLayer<dll::ILayer2*>()->getStationType() == wns::service::dll::StationTypes::UT(),
+           "Destination address is not valid and node is not a STA");
+
+    if((not destAdr.isValid()) and
+       (this->getFUN()->getLayer<dll::ILayer2*>()->getStationType() == wns::service::dll::StationTypes::UT()))
+    {
+        // no destination address and STA -> destination is AP
+        assure(this->getFUN()->getLayer<dll::ILayer2*>()->getControlService<dll::services::control::Association>("ASSOCIATION")->hasAssociation(),
+               "Outgoing data, but no association");
+        destAdr = this->getFUN()->getLayer<dll::ILayer2*>()->getControlService<dll::services::control::Association>("ASSOCIATION")->getAssociation();
+    }
+
+    Bit commandPoolSize;
+    Bit dataSize;
+    this->getFUN()->calculateSizes(compound->getCommandPool(), commandPoolSize, dataSize, this);
+    const long int compoundLength = commandPoolSize + dataSize;
+
+    this->putProbe(bitsOutgoingProbeHolder, destAdr.getInteger(), compoundLength);
+}
+
+void
+DestinationSortedWindowProbe::putProbe(SlidingWindowMap& probeHolder,
+                                       unsigned int adr,
+                                       double value)
+{
+    if(not probeHolder.knows(adr))
+    {
+        probeHolder.insert(adr, new wns::SlidingWindow(windowSize));
+    }
+    probeHolder.find(adr)->put(value);
+}
+
+void
+DestinationSortedWindowProbe::periodically()
+{
+
+    storeProbes(bitsIncomingProbeHolder, bitsIncoming);
+    storeProbes(bitsAggregatedProbeHolder, bitsAggregated);
+    storeProbes(bitsOutgoingProbeHolder, bitsOutgoing);
+
+    // relative goodput: aggregated/outgoing to one destination
+
+    for(SlidingWindowMap::const_iterator outItr = bitsOutgoingProbeHolder.begin();
+        outItr != bitsOutgoingProbeHolder.end();
+        ++outItr)
+    {
+        bool found = false;
+        for(SlidingWindowMap::const_iterator aggItr = bitsAggregatedProbeHolder.begin();
+            aggItr != bitsAggregatedProbeHolder.end();
+            ++aggItr)
+        {
+            if(aggItr->first == outItr->first)
+            {
+                // set the correct context
+                if(outItr->second->getPerSecond() > 0)
+                {
+                    this->getFUN()->getLayer<wifimac::Layer2*>()->updateContext("MAC.WindowProbeAddress", aggItr->first);
+                    relativeGoodput->put(aggItr->second->getPerSecond() / outItr->second->getPerSecond());
+                }
+                found = true;
+                break;
+            }
+        }
+        if(not found and (outItr->second->getPerSecond() > 0))
+        {
+            // outgoing, but no aggregated
+            this->getFUN()->getLayer<wifimac::Layer2*>()->updateContext("MAC.WindowProbeAddress", outItr->first);
+            relativeGoodput->put(0.0);
+        }
+    }
+
+    // Call base class function
+    this->wns::ldk::probe::bus::Window::periodically();
+}
+
+void
+DestinationSortedWindowProbe::storeProbes(SlidingWindowMap& probeHolder, wns::probe::bus::ContextCollectorPtr& putter)
+{
+    for(SlidingWindowMap::const_iterator probeItr = probeHolder.begin(); probeItr != probeHolder.end(); ++probeItr)
+    {
+        // set the correct context
+        this->getFUN()->getLayer<wifimac::Layer2*>()->updateContext("MAC.WindowProbeAddress", probeItr->first);
+
+        putter->put(probeItr->second->getPerSecond());
+    }
+}
---
+++ src/helper/DestinationSortedWindowProbe.hpp
@@ -0,0 +1,104 @@
+/******************************************************************************
+ * WiFiMac                                                                    *
+ * This file is part of openWNS (open Wireless Network Simulator)
+ * _____________________________________________________________________________
+ *
+ * Copyright (C) 2004-2007
+ * Chair of Communication Networks (ComNets)
+ * Kopernikusstr. 16, D-52074 Aachen, Germany
+ * phone: ++49-241-80-27910,
+ * fax: ++49-241-80-22242
+ * email: info@openwns.org
+ * www: http://www.openwns.org
+ * _____________________________________________________________________________
+ *
+ * openWNS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 2 as published by the
+ * Free Software Foundation;
+ *
+ * openWNS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#ifndef WIFIMAC_HELPER_DESTINATIONSORTEDWINDOWPROBE_HPP
+#define WIFIMAC_HELPER_DESTINATIONSORTEDWINDOWPROBE_HPP
+
+#include <WNS/ldk/probe/bus/Window.hpp>
+#include <WNS/probe/bus/ContextCollector.hpp>
+#include <WNS/SlidingWindow.hpp>
+#include <WNS/ldk/CommandReaderInterface.hpp>
+#include <WNS/container/Registry.hpp>
+
+#include <vector>
+
+namespace wifimac { namespace helper {
+
+    /**
+	 * @brief FunctionalUnit to probe windowing throughputs that include the
+	 * destination of the frame as context
+	 *
+	 * This is a derivate of the wns::ldk::probe::Window
+	 *
+	 */
+    class DestinationSortedWindowProbe:
+        public wns::ldk::probe::bus::Window
+    {
+        typedef wns::container::Registry<unsigned int, wns::SlidingWindow*, wns::container::registry::DeleteOnErase> SlidingWindowMap;
+
+    public:
+        DestinationSortedWindowProbe(wns::ldk::fun::FUN* fuNet, const wns::pyconfig::View& config);
+        virtual ~DestinationSortedWindowProbe();
+
+        // Processor interface
+        virtual void processIncoming(const wns::ldk::CompoundPtr& compound);
+        virtual void processOutgoing(const wns::ldk::CompoundPtr& compound);
+
+    private:
+        virtual void onFUNCreated();
+
+        void
+        putProbe(SlidingWindowMap& probeHolder,
+                 unsigned int adr,
+                 double value);
+
+        /** @brief Periodically storing of the slidingWindow results */
+        void
+        periodically();
+
+        /** @brief Helper for periodically: store all probes in the given
+         *   sliding windows into the putters
+         */
+        void
+        storeProbes(SlidingWindowMap& probeHolder, wns::probe::bus::ContextCollectorPtr& putter);
+
+        /** @brief Access the source and destination*/
+        wns::ldk::CommandReaderInterface* ucReader;
+
+        wns::pyconfig::View config;
+        const simTimeType windowSize;
+        const std::string ucCommandName;
+
+        /** @brief Store all putter in a registry, sorted by address, created
+         *	on-demand */
+        wns::probe::bus::ContextCollectorPtr bitsIncoming;
+        wns::probe::bus::ContextCollectorPtr bitsAggregated;
+        wns::probe::bus::ContextCollectorPtr bitsOutgoing;
+        wns::probe::bus::ContextCollectorPtr relativeGoodput;
+
+        /** @brief Store all probes(sliding windows) in a registry, sorted by address, created
+         *	on-demand */
+        SlidingWindowMap bitsIncomingProbeHolder;
+        SlidingWindowMap bitsAggregatedProbeHolder;
+        SlidingWindowMap bitsOutgoingProbeHolder;
+    };
+
+} // Helper
+} // WiFiMac
+
+#endif
